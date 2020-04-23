fadee4e2adf06fbce782d4eb0b8dc4be66c0493b
wesley4546 <wesley.gardiner4546@gmail.com>
2020-04-23 15:12:06 -0400

    added lastupdated header

--- ui.R
+++ ui.R
@@ -5,16 +5,20 @@ source("R/covid_source_file.R", local = TRUE)
 
 ui <- fluidPage(
   h1("COVID-19 Death Tracker"),
+  h5(paste("Last Updated:",format(Sys.time(), "%a %b %d %X %Y"))),
   sidebarLayout(
-    
     sidebarPanel(
-      checkboxGroupInput(inputId = "state_filter", "State",
-                         choices = unique(state_longer_elections$state), selected = c("Florida", "California")),
+      checkboxGroupInput(
+        inputId = "state_filter",
+        "State",
+        choices = unique(state_longer_elections$state),
+        selected = c("Florida", "California")
+      ),
       
       actionButton(inputId = "reset_filter", "Reset Filter")
     ),
     
     mainPanel(plotOutput("lineplot"))
     
-  )
-)
+    
+  ))
